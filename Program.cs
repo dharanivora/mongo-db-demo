45b20873b0505a22daa0815566030d37b13fef49
Dharani Vora <dharanivora@outlook.com>
2023-06-26 15:28:01 -0400

    chore: connect to the db

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Mongo!");
+
+        // Connect to the db.
+        var db = new MongoCRUD("AddressBook");
+
+        Console.ReadLine();
+    }
+}
+
+public class MongoCRUD
+{
+    private readonly IMongoDatabase _db;
+
+    public MongoCRUD(string dbName)
+    {
+        var client = new MongoClient("mongodb://localhost:27017");
+        _db = client.GetDatabase(dbName);
+    }
+
+}
