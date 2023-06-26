e5a532df1059dd78951e281ae6876fcffac9f4c2
Dharani Vora <dharanivora@outlook.com>
2023-06-26 15:36:08 -0400

    chore: implement record insertion

--- Program.cs
+++ Program.cs
@@ -4,11 +4,20 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        Console.WriteLine("The app started running...");
         Console.WriteLine("Hello, Mongo!");
 
         // Connect to the db.
         var db = new MongoCRUD("AddressBook");
 
+        // Insert data into the db.
+        db.InsertRecord("Persons", new Person
+        {
+            FirstName = "Cherry",
+            LastName = "Blueberry"
+        });
+
+        Console.WriteLine("The app finished running...");
         Console.ReadLine();
     }
 }
@@ -22,5 +31,14 @@ public class MongoCRUD
         var client = new MongoClient("mongodb://localhost:27017");
         _db = client.GetDatabase(dbName);
     }
+
+    // Create data in the db.
+    public void InsertRecord<T>(string tableName, T record)
+    {
+        var collection = _db.GetCollection<T>(tableName);
+        collection.InsertOne(record);
+    }
+
+
 
 }
